acc35aae5ca1d251702fa7355d360a2b67a3e526
Stefan Dangl <sd@mobiworx.de>
2016-07-08 09:40:35 +0200

    bugfix tour-resumption

--- routing/base/followed_polyline.cpp
+++ routing/base/followed_polyline.cpp
@@ -124,7 +124,7 @@ Iter FollowedPolyline::GetClosestProjection(
   size_t const count = m_poly.GetSize() - 1;
   bool jumpableFastForward = false;
   int fwdCnt = 0;
-  int resFwdCnt = -1; // -1 will be MAX-INT because it is unsigned
+  int resFwdCnt = -1;
   bool couldJump=false; // this has to do with route-jumping (not tour-jumping, ie. not with nonff-geometry-intervals)
   for (size_t i = cur; i < count; ++i)
   {
@@ -156,8 +156,9 @@ Iter FollowedPolyline::GetClosestProjection(
                 if (doContinueTourHere){
                     LOG( my::LINFO, ("continuing tour at",i,"instead of",cur) );
                     doContinueTourHere=false;
-                    // a possible previous continuation-point has to be ignored!
+                    // a possible previous continuation-point has to be ignored! reset variables as if there was no previous point!
                     minDist = numeric_limits<double>::max();
+                    resFwdCnt = -1;
                 }else{
                     //LOG(my::LINFO,("not fast-forwarding ",(i-cur)," steps (>"+maxFastForward+") since ",nonFF.min,"<=",i,"<",nonFF.max));
                     skipTo=nonFF.max;
@@ -180,6 +181,7 @@ Iter FollowedPolyline::GetClosestProjection(
     double const dp = distFn(it);
     if ( dp < minDist ) {
       // do only jump forward by a maximum of 2 or if we could have jumped forward for quite some time now.
+      // or if this is the first available point (i.e. resFwdCnt<0)
       if (resFwdCnt<0 || fwdCnt<=resFwdCnt+2 || m_couldHaveJumped>=5) {
           //LOG(my::LINFO,("jumping forward from",resFwdCnt,"to",fwdCnt,". could have jumped before:",m_couldHaveJumped));
           m_couldHaveJumped = 0;
